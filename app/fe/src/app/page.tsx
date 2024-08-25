2f30b6fd4b52425aba5aad69fd80dfedbe4810e9
ziweek <alex.jiuk.kim@gmail.com>
2024-08-25 21:42:51 +0900

    💄 update install toast

--- app/fe/src/app/page.tsx
+++ app/fe/src/app/page.tsx
@@ -2,17 +2,15 @@
 
 import { Button, Card } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { useIsMobile } from "@/hook/useMediaQuery";
 import Footer from "@/components/footer";
 
-import toast, { Toaster } from "react-hot-toast";
+import toast, { Toaster, useToasterStore } from "react-hot-toast";
+import { IconCheck, IconNo } from "@/components/common/icon";
 
-import "react-toastify/dist/ReactToastify.css";
-import { IconGithub } from "@/components/common/icon";
-
-import Swal from "sweetalert2";
+import Image from "next/image";
 
 // global.d.ts
 declare global {
@@ -30,6 +28,8 @@ export interface BeforeInstallPromptEvent extends Event {
   prompt(): Promise<void>;
 }
 
+const TOAST_LIMIT = 1;
+
 export default function Home() {
   const router = useRouter();
   const isMobile = useIsMobile();
@@ -37,6 +37,9 @@ export default function Home() {
   const [deferredPrompt, setDeferredPrompt] = useState<
     BeforeInstallPromptEvent | undefined
   >(undefined);
+  const [isHydrated, setIsHydrated] = useState(false);
+
+  const { toasts } = useToasterStore();
 
   useEffect(() => {
     const checkResize = () => {
@@ -48,18 +51,6 @@ export default function Home() {
     };
 
     checkResize();
-    toast(
-      <p className="text-sm font-bold">
-        FY25 USFK Holiday Schedule v2 has been successfully added to our
-        service!
-      </p>,
-      {
-        icon: <IconGithub width={50}></IconGithub>,
-        style: {
-          // borderRadius: "50px",
-        },
-      }
-    );
   }, [isMobile]);
 
   const checkUnsupportedBrowser = () => {
@@ -72,44 +63,60 @@ export default function Home() {
         userAgent.indexOf("seamonkey") <= -1)
     );
   };
-
   const promptAppInstall = async () => {
     const isUnsupportedBrowser = checkUnsupportedBrowser();
+
+    if (isUnsupportedBrowser) {
+      toast.error(
+        <div className="flex flex-col w-[350px] select-none">
+          <p className="text-sm font-bold">
+            Your browser does not support one-click PWA installation.
+          </p>
+          <div className="flex w-full flex-col items-end">
+            <Button
+              variant={"light"}
+              className="text-xs w-fit h-fit p-1"
+              isIconOnly
+              disableAnimation
+              disableRipple
+              color={"primary"}
+              onPress={() => {
+                window.open(
+                  "https://amplified-purpose-11c.notion.site/My-Dear-Pass-USFK-9e714a1605a146dca142ae93c9824912?pvs=74"
+                );
+              }}
+            >
+              Move to How to Use
+            </Button>
+          </div>
+        </div>,
+        { icon: <IconNo width={50} fill="#f31260"></IconNo> }
+      );
+    }
 
     if (!isUnsupportedBrowser) {
       if (deferredPrompt) {
         deferredPrompt.prompt();
-        await deferredPrompt.userChoice;
+        deferredPrompt.userChoice;
         setDeferredPrompt(undefined);
       } else {
-        Swal.fire({
-          title: "<div style='font-size:20px;'>" + "Thank you!" + "</div>",
-          text: "You have already installed this application. 👍",
-          icon: "success",
-          confirmButtonText: "Cool",
-        });
+        toast.success(
+          <div className="flex flex-col w-[350px] select-none">
+            <p className="text-sm font-bold">
+              You have already installed our application. 👍
+            </p>
+          </div>,
+          { icon: <IconCheck width={50} fill="#17c964"></IconCheck> }
+        );
       }
-    }
-
-    if (isUnsupportedBrowser) {
-      Swal.fire({
-        title:
-          "<div style='font-size:20px;'>" +
-          "Your browser does not support direct PWA Installation." +
-          "</div>",
-        text: "So, please follow the bellow instructions.",
-        icon: "info",
-        confirmButtonText: "Okay",
-        footer:
-          '<a href="https://amplified-purpose-11c.notion.site/My-Dear-Pass-USFK-9e714a1605a146dca142ae93c9824912?pvs=74">Move to "How to Use"</a>',
-      });
     }
   };
 
   useEffect(() => {
-    const handleBeforeInstallPrompt = (e: BeforeInstallPromptEvent) => {
+    const handleBeforeInstallPrompt = async (e: BeforeInstallPromptEvent) => {
       e.preventDefault();
       setDeferredPrompt(e);
+      setIsHydrated(true);
     };
 
     window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
@@ -120,6 +127,7 @@ export default function Home() {
         .then((reg) => console.log("sw worker registered", reg))
         .catch(() => console.log("failed"));
     }
+
     return () => {
       window.removeEventListener(
         "beforeinstallprompt",
@@ -127,6 +135,62 @@ export default function Home() {
       );
     };
   }, []);
+
+  useEffect(() => {
+    toasts
+      .filter((t) => t.visible) // Only consider visible toasts
+      .filter((_, i) => i >= TOAST_LIMIT) // Is toast index over limit?
+      .forEach((t) => toast.dismiss(t.id)); // Dismiss – Use toast.remove(t.id) for no exit animation
+  }, [toasts]);
+
+  useEffect(() => {
+    toast.custom(
+      (t) => (
+        <div
+          className={`${
+            t.visible ? "animate-enter" : "animate-leave"
+          } max-w-md w-[350px] bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5 select-none`}
+        >
+          <div className="flex w-full p-4">
+            <div className="flex items-start">
+              <div className="flex-shrink-0 pt-0.5">
+                <Image
+                  className="h-10 w-10 rounded-full"
+                  src="/logo/logo-icon.png"
+                  width={100}
+                  height={100}
+                  alt="logo"
+                />
+              </div>
+              <div className="ml-3 flex-1">
+                <p className="text-sm text-gray-900 font-bold">
+                  Add to Home Screen
+                </p>
+                <p className="mt-1 text-xs text-gray-500">
+                  This web application supports one-click PWA installation.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center w-fit pr-4">
+            <Button
+              size={"sm"}
+              radius={"full"}
+              variant={"solid"}
+              onPress={async () => {
+                await promptAppInstall();
+                await toast.dismiss(t.id);
+              }}
+              className=""
+            >
+              Install
+            </Button>
+          </div>
+        </div>
+      ),
+      { id: "installation", position: "bottom-center", duration: Infinity }
+    );
+  }, [isHydrated]);
 
   return (
     <>
@@ -140,18 +204,23 @@ export default function Home() {
           <div
             className="flex flex-col items-center justify-center w-full h-full bg-cover bg-center bg-blend-lighten bg-white/20"
             style={{ backgroundImage: `url(/image/3d-deer.png)` }}
-            aria-label="상장 공장 배너"
+            aria-label="deer"
           ></div>
         )}
         {/* 1. 프로젝트 소개  */}
         <div
           className={`${
-            mobile ? "bg-center bg-cover bg-blend-lighten bg-white/50" : ""
+            mobile
+              ? "bg-center bg-cover bg-white/50 bg-blend-lighten dark:bg-black/50 dark:bg-blend-darken"
+              : ""
           } mx-auto flex h-screen flex-col items-center justify-center min-w-[300px] bg-center`}
           style={{
             backgroundImage: mobile ? `url('/image/3d-deer.png')` : "",
           }}
         >
+          {mobile && (
+            <div className="fixed top-0 bg-gradient-to-b from-white dark:from-black to-transparent h-[250px] w-full"></div>
+          )}
           <div className="z-20 flex w-full flex-col items-center justify-center space-y-8">
             {/* 소개 텍스트 */}
             <div className="flex w-full flex-col items-center justify-center gap-2">
@@ -169,7 +238,7 @@ export default function Home() {
             <div className="flex w-full flex-col items-center justify-center space-y-4">
               <div className="flex flex-row space-x-2">
                 <Button
-                  className={`font-bold border-black hover:-translate-y-1 ${
+                  className={`font-bold border-black dark:border-white hover:-translate-y-1 ${
                     mobile ? "border-2" : "border-3"
                   }`}
                   size={mobile ? "md" : "lg"}
@@ -185,7 +254,7 @@ export default function Home() {
                   How to use
                 </Button>
                 <Button
-                  className={`font-bold border-black hover:-translate-y-1 ${
+                  className={`font-bold border-black dark:border-white hover:-translate-y-1 ${
                     mobile ? "border-2" : "border-3"
                   }`}
                   size={mobile ? "md" : "lg"}
@@ -199,15 +268,6 @@ export default function Home() {
                   Get Started
                 </Button>
               </div>
-              {!deferredPrompt && (
-                <Button
-                  onPress={promptAppInstall}
-                  variant={"light"}
-                  className="font-bold underline underline-offset-4"
-                >
-                  Add to Home Screen
-                </Button>
-              )}
             </div>
           </div>
 
@@ -220,7 +280,15 @@ export default function Home() {
           </div> */}
         </div>
       </section>
-      <Toaster />
+      <Toaster
+        position={"bottom-center"}
+        reverseOrder={false}
+        toastOptions={{
+          style: {
+            maxWidth: 350,
+          },
+        }}
+      />
     </>
   );
 }
--- app/fe/src/app/provider.tsx
+++ app/fe/src/app/provider.tsx
@@ -3,23 +3,22 @@
 // import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { NextUIProvider } from "@nextui-org/react";
 import { Component, useEffect, useState } from "react";
-// import { ThemeProvider as NextThemesProvider } from "next-themes";
-// import { useTheme } from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useTheme } from "next-themes";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // const queryClient = new QueryClient();
 
 export default function AppProvider({ children }: any) {
-  //   const { systemTheme } = useTheme();
+  const { systemTheme } = useTheme();
 
   return (
     // <QueryClientProvider client={queryClient}>
     //   <ReactQueryDevtools initialIsOpen={true} />
     <NextUIProvider>
-      {/* <NextThemesProvider attribute="class" defaultTheme={"light"}> */}
-
-      {children}
-      {/* </NextThemesProvider> */}
+      <NextThemesProvider attribute="class" defaultTheme={"light"}>
+        {children}
+      </NextThemesProvider>
     </NextUIProvider>
     // </QueryClientProvider>
   );
